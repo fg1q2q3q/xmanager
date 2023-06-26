2dc01284e4c6f4be7ea2ea8b40c08d139e232217
30784510@qq.com <260213qq>
2023-06-26 18:14:40 +0800

    Signed-off-by: 30784510@qq.com <260213qq>

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from 'vue'
-import pinia from './store'
-import routes from './router'
-import App from './App.vue'
-
-const app = createApp(App)
-app.use(pinia).use(routes).mount('#app')
